2130c5d5701dcc3318fcf8e1130ba537bb804bce
Yoshinari Fujinuma <fujinumay@gmail.com>
2017-11-21 14:36:26 +0900

    adding stop after returning 10 Nones

--- MTPythonSampleCode/translate_two_langs.py
+++ MTPythonSampleCode/translate_two_langs.py
@@ -35,29 +35,33 @@ def translate(finalToken, textToTranslate, fromLangCode, toLangCode):
 
 
 if __name__ == "__main__":
-    client_secret = open("client_secret.txt").readline().strip()
+    #client_secret = open("client_secret.txt").readline().strip()
+    client_secret = open("client_secret_michael.txt").readline().strip()
     #client_secret = open("client_secret_2.txt").readline().strip()
     auth_client = AzureAuthClient(client_secret)
     bearer_token = 'Bearer ' + str(auth_client.get_access_token())
 
     #text = "I like"
     #text = u"ثمة"
-    f = "/Users/yoshinarifujinuma/work/zika-paul/uniformly_sampled_zika_tweets_url_user_hashtags_del.txt"
-    f_attr = "/Users/yoshinarifujinuma/work/zika-paul/uniformly_sampled_zika_tweets_url_user_hashtags_del_attributes.txt"
+    #data_dir = "/Users/yoshinarifujinuma/work/zika-paul/"
+    data_dir = "/home/yofu1973/work/zika-paul/data/"
+    f = data_dir + "uniformly_sampled_zika_tweets_url_user_hashtags_del.txt"
+    f_attr = data_dir + "uniformly_sampled_zika_tweets_url_user_hashtags_del_attributes.txt"
 
     langs = []# list of langauges of tweets. indice i = tweet i
     for line in codecs.open(f_attr, "r", "utf-8"):
         cols = line.strip().split()
         lang = cols[0]
         langs.append(lang)
-    startline = 1
+    #startline = 1
+    startline = 2001 # TODO: be sure to update this
     #startline = 3174 + 1 # Resume at the point that was fdone
     #f_out = codecs.open(f + ".en", "w", "utf-8")
     #f_out = codecs.open(f + ".en", "a", "utf-8")
     #f_out = codecs.open(f + ".pt", "a", "utf-8")
     none_returned = 0
     for i, line in enumerate(codecs.open(f, "r", "utf-8")):
-        if i == 1000: break
+        if i == 10000: break
         from_lang = langs[i]
         to_langs = []
         # translate to two other languages
@@ -90,7 +94,7 @@ if __name__ == "__main__":
                 f_out = codecs.open(f + "." + to_lang, "a", "utf-8") # Be sure to open and write at every time
                 f_out.write("'None' returned by the API\n")
                 none_returned += 1
-                #if none_returned >= 10: break
+                if none_returned >= 10: break
         if i % 1000 == 0:
             print("finished %i lines" % i)
             
